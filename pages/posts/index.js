b0ab00eaf11bbb9506184817a8657dd2dd3c8855
MalicknND <ndiayemalick062@gmail.com>
2023-11-26 00:56:40 +0100

    next-starter

---
+++ pages/posts/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../../components/layout";
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          {" "}
+          I am a self-taught web developer passionate about creating websites
+          and applications. I started learning web development on my own and I
+          am currently a student in development to continue acquiring new
+          skills.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
