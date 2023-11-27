1c7b6aaa397c5e7af3c75a72b89a626e91fa3e1a
MalicknND <ndiayemalick062@gmail.com>
2023-11-27 23:34:17 +0100

    save work

--- components/Layout.js
+++ components/Layout.js
@@ -32,8 +32,8 @@ export default function Layout({ children, home }) {
               priority
               src="/images/me.jpg"
               className={utilStyles.borderCircle}
-              height={400}
-              width={400}
+              height={300}
+              width={300}
               alt=""
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
